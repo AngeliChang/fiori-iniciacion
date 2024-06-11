c3e739f478e1df98703b942f0c619c657fa0d98a
AngeliChang <angeli.g.chang@gmail.com>
2024-06-11 16:42:42 +0000

    First Commit on development

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("firmaname.facturas.controller.Main", {
+        onInit: function () {
+
+        }
+    });
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["firmaname/facturas/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
